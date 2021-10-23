e479348281f89875cff3610b8fe123acad13e215
Peter Schmucki <pebs@gmx.ch>
2021-10-23 23:42:08 +0200

    Tried implementing BigDecimal

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -1,10 +1,13 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.*;
 
 public class Starter {
+    // https://www.youtube.com/watch?v=O2L2Uv9pdDA&ab_channel=StatQuestwithJoshStarmer
     public static void main(String[] args) {
-        final double ALPHA = 0.5;
+        final double ALPHA = 0.001;
         final double S_THRESHOLD = 0.5;
         final int topWordsToCompare = 1000;
         final int DEBUG_ENTRIES = 15;
@@ -15,7 +18,9 @@ public class Starter {
         // Read all ham mails
         printBanner("Top Ham Words");
         Map<String, Double> hamWords = new HashMap<>();
-        readWordsAndPutCount(hamWords, "C:/Repos/bayes-spam-filter/src/main/resources/test/ham-test");
+        for (File file : returnListOfFilesInDir("C:/Repos/bayes-spam-filter/src/main/resources/test/ham-test")) {
+            readWordsAndPutCount(hamWords, file);
+        }
         showTopNEntries(hamWords, DEBUG_ENTRIES);
         printBanner("Size of Ham Words");
         System.out.println(hamWords.keySet().size());
@@ -23,7 +28,9 @@ public class Starter {
         // Read all spam mails
         printBanner("Top Spam Words");
         Map<String, Double> spamWords = new HashMap<>();
-        readWordsAndPutCount(spamWords, "C:/Repos/bayes-spam-filter/src/main/resources/test/spam-test");
+        for (File file : returnListOfFilesInDir("C:/Repos/bayes-spam-filter/src/main/resources/test/spam-test")) {
+            readWordsAndPutCount(spamWords, file);
+        }
         showTopNEntries(spamWords, DEBUG_ENTRIES);
         printBanner("Size of Spam Words");
         System.out.println(spamWords.keySet().size());
@@ -38,42 +45,79 @@ public class Starter {
         showBottomNEntries(spamWords, DEBUG_ENTRIES);
 
         // c) Calibrate
-        // Select a top number of Spam Words to compare
+
+        /**
+         * // Select a top number of Spam Words to compare
         List<Map.Entry<String, Double>> topSpamWords = getTopNEntries(spamWords, topWordsToCompare);
         // Map the word to the number of occurences of all words first in spam, than in ham
         List<Double> probabilityToOccurInSpam = topSpamWords.stream().map(entry -> {
-            return getRepresentationOfWordInMap(entry.getKey(), spamWords);
+            return getProbabilityOfWordInMap(entry.getKey(), spamWords);
         }).toList();
 
         List<Double> probabilityToOccurInHam = topSpamWords.stream().map(entry -> {
-            return getRepresentationOfWordInMap(entry.getKey(), hamWords);
-        }).toList();
+            return getProbabilityOfWordInMap(entry.getKey(), hamWords);
+        }).toList();**/
+
+        // Read all HAM Mail
+        int i = 0;
+        int j = 0;
+        Map<String, Double> validHam;
+        for (File file : returnListOfFilesInDir("C:/Repos/bayes-spam-filter/src/main/resources/valid/ham-kallibrierung")) {
+            validHam = new HashMap<>();
+            BigDecimal validHamProbabilityProduct = BigDecimal.ONE;
+            BigDecimal validSpamProbabilityProduct = BigDecimal.ONE;
+            readWordsAndPutCount(validHam, file);
+            // For each word in the dataset, calculate the probability of the words being spam or ham
+            for (Map.Entry<String, Double> e : validHam.entrySet()) {
+                validHamProbabilityProduct = validHamProbabilityProduct.multiply(
+                        getProbabilityOfWordInMap(e.getKey(), hamWords).multiply(BigDecimal.valueOf(e.getValue()))
+                );
+
+                validSpamProbabilityProduct = validSpamProbabilityProduct.multiply(
+                        getProbabilityOfWordInMap(e.getKey(), spamWords).multiply(BigDecimal.valueOf(e.getValue()))
+                );
+            }
+            BigDecimal spamProb = getSpamProbability(validSpamProbabilityProduct, validHamProbabilityProduct, BigDecimal.valueOf(S_THRESHOLD));
+            if(spamProb.doubleValue() >= 0.5) {
+                j++;
+            }
+            i++;
+            printBanner(String.format("%s: %s", file.getName(), spamProb));
+        }
+        printBanner(String.format("%s out of %s Ham Mails where categorized as spam", j, i));
+
+        // If Ham Mail failed with P(H) < 0.5, add the words to ham
+
+        // Read all Spam Mail
+        // If Spam Mail failed with P(S) < 0.5, add the words to spam
     }
 
-    public static void readWordsAndPutCount(Map<String, Double> map, String path) {
+    public static void readWordsAndPutCount(Map<String, Double> map, File file) {
         // https://stackoverflow.com/questions/1844688/how-to-read-all-files-in-a-folder-from-java
         // Read all files in dir
-        File folder = new File(path);
-        for (final File fileEntry : folder.listFiles()) {
-            if(!fileEntry.isDirectory()) {
-                // https://stackoverflow.com/questions/4574041/read-next-word-in-java
-                Scanner scanner = null;
-                try {
-                    scanner = new Scanner(fileEntry);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-                // Read line by line
-                while (scanner.hasNextLine()) {
-                    String line = scanner.nextLine();
-                    for (String word : line.split(" ")) {
-                        // Read word by word (Word is defined as everything split by a space)
-                        // https://stackoverflow.com/questions/81346/most-efficient-way-to-increment-a-map-value-in-java
-                        map.merge(word, 1.0, Double::sum);
-                    }
+        if(!file.isDirectory()) {
+            // https://stackoverflow.com/questions/4574041/read-next-word-in-java
+            Scanner scanner = null;
+            try {
+                scanner = new Scanner(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            // Read line by line
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                for (String word : line.split(" ")) {
+                    // Read word by word (Word is defined as everything split by a space)
+                    // https://stackoverflow.com/questions/81346/most-efficient-way-to-increment-a-map-value-in-java
+                    map.merge(word, 1.0, Double::sum);
                 }
             }
         }
+    }
+
+    public static List<File> returnListOfFilesInDir(String path) {
+        File folder = new File(path);
+        return Arrays.stream(folder.listFiles()).toList();
     }
 
     public static List<Map.Entry<String, Double>> getTopNEntries(Map<String, Double> map, int n) {
@@ -111,23 +155,22 @@ public class Starter {
         }
     }
 
-    public static double getRepresentationOfWordInMap(String word, Map<String, Double> map) {
-        // System.out.println(String.format("Word: %s, size: %s", word, map.entrySet().size()));
-        return map.get(word) / map.entrySet().size();
+    public static BigDecimal getProbabilityOfWordInMap(String word, Map<String, Double> map) {
+        // https://stackoverflow.com/questions/39851350/reducing-map-by-using-java-8-stream-api
+        double countOfWords = map.values().stream().reduce(0d, (a, b) -> a + b);
+        System.out.println(countOfWords);
+        if(map.containsKey(word)) {
+            return BigDecimal.ONE.divide(BigDecimal.valueOf(countOfWords), 2, RoundingMode.HALF_UP);
+        }
+        return BigDecimal.ONE;
     }
 
-    public static double getProbabilityOfTopNWordsToBeContainedInSpam(double spamThreshold, List<Double> spamProbability, List<Double> hamProbability) {
-        double hamThreshold = 1 - spamThreshold;
-        // https://stackoverflow.com/questions/36833932/how-to-multiply-values-in-a-list-using-java-8-streams/36836942
-        // P(S|topNWords)
-        return spamThreshold * reduceListToProduct(spamProbability) / ((spamThreshold * reduceListToProduct(spamProbability)) + (hamThreshold * reduceListToProduct(hamProbability)));
-    }
-
-    public static double getProbabilityOfTopNWordsToBeContainedInHam(double spamThreshold, List<Double> spamProbability, List<Double> hamProbability) {
-        double hamThreshold = 1 - spamThreshold;
-        // https://stackoverflow.com/questions/36833932/how-to-multiply-values-in-a-list-using-java-8-streams/36836942
-        // P(H|topNWords)
-        return hamThreshold * reduceListToProduct(hamProbability) / ((spamThreshold * reduceListToProduct(spamProbability)) + (hamThreshold * reduceListToProduct(hamProbability)));
+    public static BigDecimal getSpamProbability(BigDecimal spamProb, BigDecimal hamProb, BigDecimal spamThreshold) {
+        BigDecimal hamThreshold = BigDecimal.ONE.subtract(spamThreshold);
+        // Divide the spam bias * spam values by (spam bias * spam values) + (ham bias * ham values)
+        return (spamProb.multiply(spamThreshold).divide(
+                        (spamProb.multiply(spamThreshold).add(hamProb.multiply(hamThreshold))),
+                2, RoundingMode.HALF_UP));
     }
 
     public static double reduceListToProduct(List<Double> list) {
